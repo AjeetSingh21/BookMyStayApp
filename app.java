8cc397dc6b7d454e5ff6936fed1996c05e1474a5
Ajeet Singh <ajeet.singh77@outlook.com>
2026-03-23 15:37:51 +0530

    UC4 added by ajeet singh

--- app.java
+++ app.java
@@ -1,59 +1,90 @@
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.HashMap;
+import java.util.Map;
 
-public class App {
+public class app {
+
+    public static abstract class Room {
+        protected int numberOfBeds;
+        protected int squareFeet;
+        protected double pricePerNight;
+
+        public Room(int numberOfBeds, int squareFeet, double pricePerNight) {
+            this.numberOfBeds = numberOfBeds;
+            this.squareFeet = squareFeet;
+            this.pricePerNight = pricePerNight;
+        }
+
+        public void displayRoomDetails() {
+            System.out.println("Beds: " + numberOfBeds);
+            System.out.println("Size: " + squareFeet + " sqft");
+            System.out.println("Price per night: " + pricePerNight);
+        }
+    }
+
+    public static class SingleRoom extends Room {
+        public SingleRoom() {
+            super(1, 250, 1500.0);
+        }
+    }
+
+    public static class DoubleRoom extends Room {
+        public DoubleRoom() {
+            super(2, 400, 2500.0);
+        }
+    }
+
+    public static class Suite extends Room {
+        public Suite() {
+            super(3, 750, 5000.0);
+        }
+    }
+
+    public static class RoomInventory {
+        private Map<String, Integer> roomAvailability;
+
+        public RoomInventory() {
+            roomAvailability = new HashMap<>();
+        }
+
+        public void initializeInventory(String roomType, int available) {
+            roomAvailability.put(roomType, available);
+        }
+
+        public Map<String, Integer> getRoomAvailability() {
+            return roomAvailability;
+        }
+    }
 
     public static void main(String[] args) {
-        BookingRequestQueue queue = new BookingRequestQueue();
 
-        queue.addRequest(new Reservation("R001", "Alice"));
-        queue.addRequest(new Reservation("R002", "Bob"));
-        queue.addRequest(new Reservation("R003", "Charlie"));
+        RoomInventory inventory = new RoomInventory();
+        inventory.initializeInventory("Single Room", 5);
+        inventory.initializeInventory("Double Room", 3);
+        inventory.initializeInventory("Suite Room", 2);
 
-        queue.displayRequests();
-    }
-}
+        System.out.println("Room Search\n");
 
-class Reservation {
-    private String reservationId;
-    private String guestName;
+        Map<String, Integer> data = inventory.getRoomAvailability();
 
-    public Reservation(String reservationId, String guestName) {
-        this.reservationId = reservationId;
-        this.guestName = guestName;
-    }
+        if (data.get("Single Room") > 0) {
+            System.out.println("Single Room:");
+            SingleRoom s = new SingleRoom();
+            s.displayRoomDetails();
+            System.out.println("Available: " + data.get("Single Room") + "\n");
+        }
 
-    public String getReservationId() {
-        return reservationId;
-    }
+        if (data.get("Double Room") > 0) {
+            System.out.println("Double Room:");
+            DoubleRoom d = new DoubleRoom();
+            d.displayRoomDetails();
+            System.out.println("Available: " + data.get("Double Room") + "\n");
+        }
 
-    public String getGuestName() {
-        return guestName;
-    }
-
-    public String toString() {
-        return reservationId + " - " + guestName;
-    }
-}
-
-class BookingRequestQueue {
-    private Queue<Reservation> requestQueue;
-
-    public BookingRequestQueue() {
-        requestQueue = new LinkedList<>();
-    }
-
-    public void addRequest(Reservation reservation) {
-        requestQueue.offer(reservation);
-    }
-
-    public Reservation processRequest() {
-        return requestQueue.poll();
-    }
-
-    public void displayRequests() {
-        for (Reservation r : requestQueue) {
-            System.out.println(r);
+        if (data.get("Suite Room") > 0) {
+            System.out.println("Suite Room:");
+            Suite suite = new Suite();
+            suite.displayRoomDetails();
+            System.out.println("Available: " + data.get("Suite Room"));
         }
     }
 }
